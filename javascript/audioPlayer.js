d59340b07efb3588249ba6200875f9bae531757f
Judah Avery <judahbavery@gmail.com>
2022-01-02 16:19:41 -0500

    Fixed all the broken stuff I created in the AudioPlayer

--- javascript/audioPlayer.js
+++ javascript/audioPlayer.js
@@ -3,7 +3,7 @@ const audioParent = document.querySelector('.master-audio-cont');
 const audioPlayer = document.querySelector('.audio-player');
 const audioProgressCont = document.querySelector('.audio-progress-cont');
 const audioProgress = document.querySelector('.audio-progress');
-const mainPlayBtn = document.querySelector('.controller').querySelector('#play-button');
+const mainPlayBtn = document.querySelector('.main-play');
 const skip = document.querySelector('#skip');
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
@@ -12,6 +12,7 @@ const title = document.querySelector('.songTitle');
 const writers = document.querySelector('.songWriters');
 const volProgressCont = document.querySelector('.volume-progress-cont');
 const volProgress = document.querySelector('.volume-progress');
+var activePlayBtn;
 
 class Song {
 
@@ -30,35 +31,59 @@ const songs = [
     new Song("Doing That (Clip)", "doingThat_short.mp3", "Adam Avery, Other Person, Other Person, Other Person"),
     new Song("Tell Me (Clip)", "tellMe_short.mp3", "Adam Avery, Other Person, Other Person, Other Person"),
     new Song("Don't Worry (Clip)", "dontWorry_short.mp3", "Adam Avery, Other Person, Other Person, Other Person"),
-    new Song("The Languishing (Clip)", "theLanguishing_short.mp3", "Adam Avery, Other Person, Other Person, Other Person"),
+    new Song("The Languishing (Clip)", "theLanguishing_short.mp3", "Adam Avery, Other Person, Other Person, Other Person")
 
     // Full Songs
     // EX: new Song("Name", "Writers", "song.mp3"),
 ];
 
-
 // Song Count (keep track)
-let songIndex = 3;
+let songIndex = 2;
+
+// Fixing IMG heights in the home section
+window.onresize = updateAlbumImages();
+
+function updateAlbumImages() {
+    var albums = document.querySelectorAll('.img-row-cont');
+
+    for (i = 0; i < albums.length; i++) {
+        albums[i].style.height = getComputedStyle(albums[i]).width;
+        console.log("FIXED");
+    }
+}
 
 // Load song info on the DOM first
 loadSong(songs[songIndex]);
 
 // Music Functions
-function playBtnPress(playBtn) {
+function playBtnPress(playBtn, newSongIndex) {
     const isPlaying = audioParent.classList.contains('playing');
 
     if (isPlaying) {
-        pauseSong(playBtn);
+        // If music is already playing, and we select the same song
+        // then we can pause the music
+        if (songIndex == newSongIndex) {
+            pauseSong(playBtn);
+        }
+        // If it's a different song, then we play that one instead
+        // and reset the rest
+        else {
+            playSong(playBtn, newSongIndex);
+        }
     }
     else {
-        playSong(playBtn);
+        playSong(playBtn, newSongIndex);
     }
+
+    playButtonFilter(playBtn);
 }
 
 function loadSong(song) {
     title.innerText = song.songTitle;
     writers.innerText = song.songWriters;
     audio.src = "../audio/" + song.songFile + ".mp3";
+
+    activeSong = song;
 }
 
 // Clear all play buttons when a play button is pressed
@@ -72,7 +97,9 @@ function resetAllPlayBtns() {
     }
 }
 
-function playSong(playBtn) {
+function playSong(playBtn, newSongIndex) {
+
+
     // Update audio-player and the clicked button
     audioPlaylist.classList.add('playing');
     audioParent.classList.add('playing');
@@ -94,11 +121,13 @@ function pauseSong(playBtn) {
     playBtn.querySelector('i.fas').classList.add('fa-play');
 }
 
-// Fixing IMG heights in the home section
-window.onresize = function updateAlbumImages() {
-    var albums = document.querySelectorAll('.img-row-cont');
-
-    for (i = 0; i < albums.length; i++) {
-        albums[i].style.height = getComputedStyle(albums[i]).width;
+// This only saves buttons that are NOT the mainPlayBtn as the activePlayBtn
+function playButtonFilter (pressedBtn) {
+    if (pressedBtn == mainPlayBtn) {
+        // Main play button was pressed so we do nothing
+        console.log("MAIN PLAY BUTTON WAS PRESSED, DO NOTHING");
+    }
+    else {
+        activePlayBtn = pressedBtn;
     }
 }
